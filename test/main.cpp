2531a24276eb87e6eda1923b473f90b8afb80844
djowel <djowel@gmail.com>
2020-03-12 22:56:16 +0800

    arcs

--- test/main.cpp
+++ test/main.cpp
@@ -25,8 +25,20 @@ void basics(canvas& cnv)
     cnv.line_to({ 100, 100 });
     cnv.stroke();
 
+    cnv.line_width(4);
     cnv.circle({{ 120, 80 }, 40});
     cnv.fill_style(colors::gold.opacity(0.5));
+    cnv.stroke_style(colors::gold);
+    cnv.fill_preserve();
+    cnv.stroke();
+
+    cnv.line_width(2);
+    cnv.begin_path();
+    cnv.move_to({ 100, 20 });
+    cnv.arc_to({ 150, 20 }, { 150, 70 }, 30);
+    cnv.fill_style(colors::blue_violet.opacity(0.4));
+    cnv.stroke_style(colors::blue_violet);
+    cnv.stroke_preserve();
     cnv.fill();
 }
 
@@ -35,7 +47,7 @@ void draw(canvas& cnv)
     basics(cnv);
 
     cnv.scale({ 1.5, 1.5 });
-    cnv.translate({ 100, 0 });
+    cnv.translate({ 150, 0 });
     cnv.rotate(0.8);
     basics(cnv);
 }
