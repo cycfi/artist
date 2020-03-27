193c2685dc401a35b34ac73d3ac0e6d95abd9ebb
djowel <djowel@gmail.com>
2020-03-27 23:44:45 +0800

    Skia text layout work in progress

--- lib/impl/skia/detail/harfbuzz.cpp
+++ lib/impl/skia/detail/harfbuzz.cpp
@@ -4,37 +4,45 @@
    Distributed under the MIT License [ https://opensource.org/licenses/MIT ]
 =============================================================================*/
 #include "harfbuzz.hpp"
+#include <hb.h>
+#include <hb-ot.h>
+#include <SkStream.h>
+#include <SkTypeface.h>
 
 namespace cycfi::artist::detail
 {
    hb_blob::hb_blob(std::unique_ptr<SkStreamAsset> asset)
    {
       std::size_t size = asset->getLength();
-      hb_blob_t* blob;
       if (const void* base = asset->getMemoryBase())
       {
-         blob = hb_blob_create(
+         _blob = hb_blob_create(
             (char*)base
-            , SkToUInt(size)
-            , HB_MEMORY_MODE_READONLY
-            , asset.release()
-            , [](void* p) { delete (SkStreamAsset*)p; }
+           , SkToUInt(size)
+           , HB_MEMORY_MODE_READONLY
+           , asset.release()
+           , [](void* p) { delete (SkStreamAsset*)p; }
          );
       }
       else
       {
          void* ptr = size ? std::malloc(size) : nullptr;
          asset->read(ptr, size);
-         blob = hb_blob_create(
+         _blob = hb_blob_create(
             (char*)ptr
-            , SkToUInt(size)
-            , HB_MEMORY_MODE_READONLY
-            , ptr
-            , std::free
+           , SkToUInt(size)
+           , HB_MEMORY_MODE_READONLY
+           , ptr
+           , std::free
          );
       }
-      SkASSERT(blob);
-      hb_blob_make_immutable(blob);
+      SkASSERT(_blob);
+      hb_blob_make_immutable(_blob);
+   }
+
+   hb_blob::~hb_blob()
+   {
+      hb_blob_destroy(_blob);
    }
 
    hb_font::hb_font(SkTypeface* tf)
@@ -48,28 +56,75 @@ namespace cycfi::artist::detail
          hb_face_set_index(face, unsigned(index));
          hb_face_set_upem(face, tf->getUnitsPerEm());
 
-         font = hb_font_create(face);
-         SkASSERT(font);
-         if (font)
+         _font = hb_font_create(face);
+         SkASSERT(_font);
+         if (_font)
          {
-            hb_ot_font_set_funcs(font);
-         }
-         hb_face_destroy(face);
-         int axis_count = tf->getVariationDesignPosition(nullptr, 0);
-
-         if (axis_count > 0)
-         {
-            SkAutoSTMalloc<4, SkFontArguments::VariationPosition::Coordinate> axis_values(axis_count);
-            if (tf->getVariationDesignPosition(axis_values, axis_count) == axis_count)
+            hb_ot_font_set_funcs(_font);
+            int axis_count = tf->getVariationDesignPosition(nullptr, 0);
+            if (axis_count > 0)
             {
-               hb_font_set_variations(
-                  font
+               SkAutoSTMalloc<4, SkFontArguments::VariationPosition::Coordinate> axis_values(axis_count);
+               if (tf->getVariationDesignPosition(axis_values, axis_count) == axis_count)
+               {
+                  hb_font_set_variations(
+                     _font
                   , reinterpret_cast<hb_variation_t*>(axis_values.get())
                   , axis_count
-               );
+                  );
+               }
             }
          }
+         else
+         {
+            _font = nullptr;
+         }
+         hb_face_destroy(face);
       }
+   }
+
+   hb_font::~hb_font()
+   {
+      hb_font_destroy(_font);
+   }
+
+   hb_buffer::hb_buffer(std::string_view utf8)
+    : _buffer(hb_buffer_create())
+   {
+      hb_buffer_add_utf8(_buffer, utf8.data(), utf8.size(), 0, utf8.size());
+   }
+
+   hb_buffer::~hb_buffer()
+   {
+      hb_buffer_destroy(_buffer);
+   }
+
+   void hb_buffer::direction(hb_direction_t dir)
+   {
+      hb_buffer_set_direction(_buffer, dir);
+   }
+
+   void hb_buffer::script(hb_script_t scr)
+   {
+      hb_buffer_set_script(_buffer, scr);
+   }
+
+   void hb_buffer::language(std::string_view lang)
+   {
+      hb_buffer_set_language(_buffer, hb_language_from_string(lang.data(), lang.size()));
+   }
+
+   void hb_buffer::shape(hb_font const& font)
+   {
+      hb_shape(font.get(), _buffer, nullptr, 0);
+   }
+
+   hb_buffer::glyphs_info hb_buffer::glyphs() const
+   {
+      glyphs_info info;
+      info.glyphs = hb_buffer_get_glyph_infos(_buffer, &info.count);
+      info.positions = hb_buffer_get_glyph_positions(_buffer, &info.count);
+      return info;
    }
 }
 
--- lib/impl/skia/detail/harfbuzz.hpp
+++ lib/impl/skia/detail/harfbuzz.hpp
@@ -3,35 +3,70 @@
 
    Distributed under the MIT License [ https://opensource.org/licenses/MIT ]
 =============================================================================*/
+#include <memory>
+#include <string_view>
 #include <hb.h>
 #include <hb-ot.h>
-#include <SkStream.h>
-#include <SkTypeface.h>
+
+struct SkStreamAsset;
+struct SkTypeface;
 
 namespace cycfi::artist::detail
 {
-   struct hb_blob
+   class hb_blob
    {
-                  hb_blob(std::unique_ptr<SkStreamAsset> asset);
-                  hb_blob(hb_blob const&) = delete;
-                  ~hb_blob()  { hb_blob_destroy(blob); }
-      hb_blob_t*  get()       { return blob; }
+   public:
+                           hb_blob(std::unique_ptr<SkStreamAsset> asset);
+                           hb_blob(hb_blob const&) = delete;
+                           ~hb_blob();
+
+      hb_blob_t*           get() const { return _blob; }
 
    private:
 
-      hb_blob_t* blob = nullptr;
+      hb_blob_t* _blob = nullptr;
    };
 
    struct hb_font
    {
-                  hb_font(SkTypeface* tf);
-                  hb_font(hb_font const&) = delete;
-                  ~hb_font()  { hb_font_destroy(font); }
-      hb_font_t*  get()       { return font; }
+   public:
+                           hb_font(SkTypeface* tf);
+                           hb_font(hb_font const&) = delete;
+                           ~hb_font();
+
+      hb_font_t*           get() const { return _font; }
 
    private:
 
-      hb_font_t* font = nullptr;
+      hb_font_t* _font = nullptr;
+   };
+
+   class hb_buffer
+   {
+   public:
+                           hb_buffer(std::string_view utf8);
+                           hb_buffer(hb_buffer const&) = delete;
+                           ~hb_buffer();
+
+      void                 direction(hb_direction_t dir);
+      void                 script(hb_script_t scr);
+      void                 language(std::string_view lang);
+
+      struct glyphs_info
+      {
+         unsigned int         count;
+         hb_glyph_info_t*     glyphs;
+         hb_glyph_position_t* positions;
+      };
+
+      void                 shape(hb_font const& font);
+      glyphs_info          glyphs() const;
+
+      hb_buffer_t*         get() const { return _buffer; }
+
+   private:
+
+      hb_buffer_t*         _buffer = nullptr;
    };
 }
 
