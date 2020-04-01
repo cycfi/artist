1de69295ad9426337f05795ca1bef96396788ccb
djowel <djowel@gmail.com>
2020-04-01 11:56:07 +0800

    correct x-platform logic

--- lib/include/artist/detail/filesystem.hpp
+++ lib/include/artist/detail/filesystem.hpp
@@ -6,12 +6,12 @@
 #if !defined(ELEMENTS_DETAIL_FILESYSTEM_APRIL_1_2020)
 #define ELEMENTS_DETAIL_FILESYSTEM_APRIL_1_2020
 
-#if defined(__apple_build_version__) && (__apple_build_version__ > 11000033L)
-# define MACOS_HAS_STD_FILESTSTEM
+#if defined(__apple_build_version__) && (__apple_build_version__ <= 11000033L)
+# define ARTIST_HAS_NO_STD_FILESTSTEM
 #endif
 
 #if defined(__cplusplus) && __cplusplus >= 201703L && defined(__has_include)
-# if __has_include(<filesystem>) && defined(MACOS_HAS_STD_FILESTSTEM)
+# if __has_include(<filesystem>) && !defined(ARTIST_HAS_NO_STD_FILESTSTEM)
 #  define GHC_USE_STD_FS
 #  include <filesystem>
    namespace fs = std::filesystem;
