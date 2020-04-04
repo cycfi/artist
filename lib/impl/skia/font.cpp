087029d9d2705e945679a38256b1b7c11a823289
Joel de Guzman <djowel@gmail.com>
2020-04-04 20:12:09 +0800

    wip unstable commit; do it on the mac

--- lib/impl/skia/font.cpp
+++ lib/impl/skia/font.cpp
@@ -10,13 +10,16 @@
 #include <SkFontMetrics.h>
 #include <SkFontMgr.h>
 #include <artist/detail/filesystem.hpp>
+#include <fontconfig/fontconfig.h>
 
 # if defined(_WIN32)
 # include <Windows.h>
 # include "sysinfoapi.h"
 # include "tchar.h"
 # include <SkTypeface_win.h>
-# include <fontconfig/fontconfig.h>
+# include <map>
+# include <mutex>
+# define ARTIST_USE_FONT_CONFIG
 # endif
 
 namespace cycfi::artist
@@ -42,62 +45,191 @@ namespace cycfi::artist
          ltrim(s);
          rtrim(s);
       }
-   }
 
-   using namespace font_constants;
-   sk_sp<SkFontMgr> font_manager;
-
-#if defined(_WIN32)
-// In Windows, we have to manually load the application fonts:
-
-   struct init_fonts
-   {
-      init_fonts()
+      inline float lerp(float a, float b, float f)
       {
-         auto fonts_path = fs::current_path() / "resources/fonts";
-
-         // Load the user fonts from the app's font folder.
-         for (fs::directory_iterator it{ fonts_path }; it != fs::directory_iterator{}; ++it)
-         {
-            auto path = it->path().wstring();
-            AddFontResource(path.c_str());
-         }
+         return (a * (1.0 - f)) + (b * f);
       }
 
-      ~init_fonts()
+      using skia_font_map_type = std::map<std::string, sk_sp<SkTypeface>>;
+
+      std::pair<skia_font_map_type&, std::mutex&> get_skia_font_map()
       {
-         auto fonts_path = fs::current_path() / "resources/fonts";
-
-         // Unload the installed user fonts from the app's font folder.
-         for (fs::directory_iterator it{ fonts_path }; it != fs::directory_iterator{}; ++it)
-         {
-            auto path = it->path().wstring();
-            RemoveFontResource(path.c_str());
-         }
+         static skia_font_map_type map_;
+         static std::mutex mutex_;
+         return { map_, mutex_ };
       }
-   };
 
-   void load_user_fonts()
-   {
-      static init_fonts init;
-   }
+      struct font_entry
+      {
+         std::string    full_name;
+         std::string    file;
+         uint8_t        weight   = font_constants::weight_normal;
+         uint8_t        slant    = font_constants::slant_normal;
+         uint8_t        stretch  = font_constants::stretch_normal;
+      };
+
+      using font_map_type = std::map<std::string, std::vector<font_entry>>;
+      font_map_type& font_map()
+      {
+         static font_map_type font_map_;
+         return font_map_;
+      }
+
+      enum
+      {
+         fc_thin            = 0,
+         fc_extralight      = 40,
+         fc_light           = 50,
+         fc_semilight       = 55,
+         fc_book            = 75,
+         fc_normal          = 80,
+         fc_medium          = 100,
+         fc_semibold        = 180,
+         fc_bold            = 200,
+         fc_extrabold       = 205,
+         fc_black           = 210
+      };
+
+      int map_fc_weight(int w)
+      {
+         auto&& map = [](double mina, double maxa, double minb, double maxb, double val)
+         {
+            return lerp(mina, maxa, (val-minb)/(maxb-minb));
+         };
+
+         namespace fc = font_constants;
+
+         if (w < fc_extralight)
+            return map(fc::thin, fc::extra_light, fc_thin, fc_extralight, w);
+         if (w < fc_light)
+            return map(fc::extra_light, fc::light, fc_extralight, fc_light, w);
+         if (w < fc_normal)
+            return map(fc::light, fc::weight_normal, fc_light, fc_normal, w);
+         if (w < fc_medium)
+            return map(fc::weight_normal, fc::medium, fc_normal, fc_medium, w);
+         if (w < fc_semibold)
+            return map(fc::medium, fc::semi_bold, fc_medium, fc_semibold, w);
+         if (w < fc_bold)
+            return map(fc::semi_bold, fc::bold, fc_semibold, fc_bold, w);
+         if (w < fc_extrabold)
+            return map(fc::bold, fc::extra_bold, fc_bold, fc_extrabold, w);
+         if (w < fc_black)
+            return map(fc::extra_bold, fc::black, fc_extrabold, fc_black, w);
+         return map(fc::black, 100, fc_black, 220, std::min(w, 220));
+      }
+
+      void init_font_map()
+      {
+         FcConfig* config = FcInitLoadConfigAndFonts();
+         auto app_fonts_path = fs::current_path() / "resources/fonts";
+         FcConfigAppFontAddDir(config, (FcChar8 const*)app_fonts_path.string().c_str());
+
+#ifdef _WIN32
+         TCHAR windir[MAX_PATH];
+         GetWindowsDirectory(windir, MAX_PATH);
+         auto system_fonts_path = fs::path(windir) / "fonts";
+         FcConfigAppFontAddDir(config, (FcChar8 const*)system_fonts_path.string().c_str());
+#endif
+
+         FcPattern*     pat = FcPatternCreate();
+         FcObjectSet*   os = FcObjectSetBuild(
+                                 FC_FAMILY, FC_FULLNAME, FC_WIDTH, FC_WEIGHT
+                               , FC_SLANT, FC_FILE, nullptr);
+         FcFontSet*     fs = FcFontList(config, pat, os);
+
+         for (int i=0; fs && i < fs->nfont; ++i)
+         {
+            FcPattern* font = fs->fonts[i];
+            FcChar8 *file, *family, *full_name;
+            if (FcPatternGetString(font, FC_FILE, 0, &file) == FcResultMatch &&
+               FcPatternGetString(font, FC_FAMILY, 0, &family) == FcResultMatch &&
+               FcPatternGetString(font, FC_FULLNAME, 0, &full_name) == FcResultMatch
+            )
+            {
+               font_entry entry;
+               entry.full_name = (const char*) full_name;
+               entry.file = (const char*) file;
+
+               int weight;
+               if (FcPatternGetInteger(font, FC_WEIGHT, 0, &weight) == FcResultMatch)
+                  entry.weight = map_fc_weight(weight); // map the weight (normalized 0 to 100)
+
+               int slant;
+               if (FcPatternGetInteger(font, FC_SLANT, 0, &slant) == FcResultMatch)
+                  entry.slant = (slant * 100) / 110; // normalize 0 to 100
+
+               int width;
+               if (FcPatternGetInteger(font, FC_WIDTH, 0, &width) == FcResultMatch)
+                  entry.stretch = (width * 100) / 200; // normalize 0 to 100
+
+               std::string key = (const char*) family;
+               trim(key);
+
+               if (auto it = font_map().find(key); it != font_map().end())
+               {
+                  it->second.push_back(entry);
+               }
+               else
+               {
+                  font_map()[key] = {};
+                  font_map()[key].push_back(entry);
+               }
+            }
+         }
+         if (fs)
+            FcFontSetDestroy(fs);
+      }
+
+      font_entry const* match(font_descr descr)
+      {
+         if (font_map().empty())
+            init_font_map();
+
+         std::istringstream str(std::string{ descr._families });
+         std::string family;
+         while (getline(str, family, ','))
+         {
+            trim(family);
+            if (auto i = font_map().find(family); i != font_map().end())
+            {
+               int min = 10000;
+               std::vector<font_entry>::const_iterator best_match = i->second.end();
+               for (auto j = i->second.begin(); j != i->second.end(); ++j)
+               {
+                  auto const& item = *j;
+
+                  // Get biased score (lower is better). Give `slant` attribute
+                  // the highest bias (3.0), followed by `weight` (1.0) and then
+                  // `stretch` (0.25).
+                  auto diff =
+                     (std::abs(int(descr._weight) - int(item.weight)) * 1.0) +
+                     (std::abs(int(descr._slant) - int(item.slant)) * 3.0) +
+                     (std::abs(int(descr._stretch) - int(item.stretch)) * 0.25)
+                     ;
+                  if (diff < min)
+                  {
+                     min = diff;
+                     best_match = j;
+                  }
+               }
+               if (best_match != i->second.end())
+                  return &*best_match;
+            }
+         }
+         return nullptr;
+      }
 
 #endif
+   }
 
    font::font()
     : _ptr(std::make_shared<SkFont>())
    {
-#if defined(_WIN32)
-      load_user_fonts();
-#endif
    }
 
    font::font(font_descr descr)
    {
-#if defined(_WIN32)
-      load_user_fonts();
-#endif
-
       if (!font_manager)
       {
 #if defined(_WIN32)
