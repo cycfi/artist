3f2322bf8af5e50798a50a8e42390b66c41c7f62
djowel <djowel@gmail.com>
2020-04-18 06:52:47 +0800

    Separate print_elapsed utility

--- examples/host/macos/skia_app.mm
+++ examples/host/macos/skia_app.mm
@@ -364,29 +364,3 @@ int run_app(
    return _app.run();
 }
 
-void print_elapsed(canvas& cnv, point br)
-{
-   static font open_sans = font_descr{ "Open Sans", 12 };
-   static int i = 0;
-   static float t_elapsed = 0;
-   static float c_elapsed = 0;
-
-   if (++i == 30)
-   {
-      i = 0;
-      c_elapsed = t_elapsed / 30;
-      t_elapsed = 0;
-   }
-   else
-   {
-      t_elapsed += elapsed_;
-   }
-
-   if (c_elapsed)
-   {
-      cnv.fill_style(rgba(220, 220, 220, 200));
-      cnv.font(open_sans);
-      cnv.text_align(cnv.right | cnv.bottom);
-      cnv.fill_text(std::to_string(1 / c_elapsed) + " fps", { br.x, br.y });
-   }
-}
