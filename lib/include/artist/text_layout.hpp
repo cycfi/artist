5c14e9f687a52437bd348b9212c3596affd255f2
djowel <djowel@gmail.com>
2020-04-23 11:02:44 +0800

    Added text color; quartz-2d for now (need to add to skia)

--- lib/include/artist/text_layout.hpp
+++ lib/include/artist/text_layout.hpp
@@ -9,6 +9,7 @@
 #include <string_view>
 #include <artist/font.hpp>
 #include <artist/rect.hpp>
+#include <artist/color.hpp>
 #include <artist/detail/utf8_utils.hpp>
 #include <memory>
 #include <functional>
@@ -22,8 +23,9 @@ namespace cycfi::artist
    {
    public:
                            text_layout(font const& font_, std::string_view utf8);
+                           text_layout(font const& font_, color c, std::string_view utf8);
                            text_layout(text_layout const& rhs) = delete;
-                           text_layout(text_layout&& rhs) noexcept = default;
+                           text_layout(text_layout&& rhs) noexcept;
                            ~text_layout();
 
       text_layout&         operator=(text_layout const& rhs) = delete;
@@ -50,6 +52,7 @@ namespace cycfi::artist
       void                 draw(canvas& cnv, point p) const;
       void                 draw(canvas& cnv, float x, float y) const;
 
+      std::size_t          num_lines() const;
       point                caret_pos(std::size_t str_pos) const;
       std::size_t          hit_test(float x, float y) const;
       std::size_t          hit_test(point p) const;
