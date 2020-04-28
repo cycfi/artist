dd76f9c757342153c5942f89ae4db40c7e01435d
djowel <djowel@gmail.com>
2020-04-28 11:34:58 +0800

    sk_app based app driver

--- examples/host/macos/skia_app.mm
+++ examples/host/macos/skia_app.mm
@@ -17,7 +17,9 @@
 #include <gl/GrGLInterface.h>
 #include <SkImage.h>
 #include <SkSurface.h>
-#include <GrBackendSurface.h>
+#include <tools/sk_app/DisplayParams.h>
+#include <tools/sk_app/WindowContext.h>
+#include <tools/sk_app/mac/WindowContextFactory_mac.h>
 #include <OpenGL/gl.h>
 
 using namespace cycfi::artist;
@@ -77,8 +79,11 @@ namespace cycfi::artist
 
 //=======================================================================
 
+using skia_context = std::unique_ptr<sk_app::WindowContext>;
+
 @interface CocoaView : NSView
 {
+   skia_context   _skia_context;
 }
 
 -(void) start;
@@ -96,10 +101,41 @@ namespace cycfi::artist
 
 - (void) start
 {
+   sk_app::window_context_factory::MacWindowInfo info;
+   info.fMainView = self;
+   _skia_context = sk_app::window_context_factory::MakeGLForMac(info, sk_app::DisplayParams());
 }
 
 - (void) drawRect : (NSRect) dirty
 {
+   sk_sp<SkSurface> backbuffer = _skia_context->getBackbufferSurface();
+   if (backbuffer)
+   {
+      SkCanvas* canvas = backbuffer->getCanvas();
+      canvas->clear(SK_ColorWHITE);
+
+      SkPaint paint;
+      paint.setAntiAlias(true);
+      paint.setColor(SK_ColorBLUE);
+      paint.setStyle(SkPaint::kStroke_Style);
+      paint.setStrokeWidth(5);
+
+      SkPath path1;
+      path1.moveTo(100, 200);
+      path1.cubicTo(100, 100, 250, 100, 250, 200);
+      path1.cubicTo(250, 300, 400, 300, 400, 200);
+
+      canvas->drawPath(path1, paint);
+
+      SkPath path2;
+      path2.moveTo(200, 300);
+      path2.quadTo(400, 50, 600, 300);
+      path2.quadTo(800, 550, 1000, 300);
+
+      canvas->drawPath(path2, paint);
+      backbuffer->flush();
+      _skia_context->swapBuffers();
+   }
 }
 
 -(BOOL) isFlipped
@@ -136,12 +172,12 @@ public:
            ];
 
       _content = [[CocoaView alloc] init];
-      [_content start];
       [_window setContentView : _content];
       [_window cascadeTopLeftFromPoint : NSMakePoint(20, 20)];
       [_window makeKeyAndOrderFront : nil];
       [_window setAppearance : [NSAppearance appearanceNamed : NSAppearanceNameVibrantDark]];
       [_window setBackgroundColor : color];
+      [_content start];
    }
 
    void start_animation()
