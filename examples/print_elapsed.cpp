5a218c458dc86fc7c02fa7e1a40b35150d18fa9d
Joel de Guzman <djowel@gmail.com>
2020-04-30 06:36:49 +0800

    Added background fill

--- examples/print_elapsed.cpp
+++ examples/print_elapsed.cpp
@@ -27,9 +27,11 @@ struct exp_moving_average
    float y = 0.0f;
 };
 
-void print_elapsed(canvas& cnv, point br, color c)
+void print_elapsed(canvas& cnv, point br, color bkd, color c)
 {
    static font open_sans = font_descr{ "Open Sans", 12 };
+   static auto metrics = open_sans.metrics();
+   static auto height = metrics.ascent + metrics.leading + metrics.descent;
    static exp_moving_average<256> ma;
    static int refresh = 0;
    static std::string fps_str;
@@ -42,8 +44,13 @@ void print_elapsed(canvas& cnv, point br, color c)
       fps_str = std::to_string(1/ave) + " fps";
    }
 
+   auto width = open_sans.measure_text(fps_str);
+   cnv.fill_style(bkd);
+   cnv.rect({ br.x-(width+4), br.y-height, br.x, br.y });
+   cnv.fill();
+
    cnv.fill_style(c);
    cnv.font(open_sans);
    cnv.text_align(cnv.right | cnv.bottom);
-   cnv.fill_text(fps_str, { br.x, br.y });
+   cnv.fill_text(fps_str, { br.x-2, br.y });
 }
