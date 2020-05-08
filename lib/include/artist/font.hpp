0586d08346e0459ce6b14865c46895cd37f12f16
djowel <djowel@gmail.com>
2020-05-08 17:49:18 +0800

    font docs

--- lib/include/artist/font.hpp
+++ lib/include/artist/font.hpp
@@ -49,15 +49,15 @@ namespace cycfi::artist
 
       enum stretch_enum
       {
-         ultra_condensed	= 25,
-         extra_condensed	= 31,
-         condensed	      = 38,
-         semi_condensed	   = 44,
-         stretch_normal	   = 50,
-         semi_expanded	   = 57,
-         expanded	         = 63,
-         extra_expanded	   = 75,
-         ultra_expanded	   = 100
+         ultra_condensed   = 25,
+         extra_condensed   = 31,
+         condensed         = 38,
+         semi_condensed    = 44,
+         stretch_normal    = 50,
+         semi_expanded     = 57,
+         expanded          = 63,
+         extra_expanded    = 75,
+         ultra_expanded    = 100
       };
    }
 
