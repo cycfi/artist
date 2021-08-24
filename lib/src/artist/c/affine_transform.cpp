17aba9025999704c3832a4531b937284488bc914
Chance Snow <git@chancesnow.me>
2021-08-24 00:22:28 -0500

    Fix `affine_transform` C API wrapper

--- lib/src/artist/c/affine_transform.cpp
+++ lib/src/artist/c/affine_transform.cpp
@@ -7,21 +7,35 @@
 #include <artist/c/affine_transform.h>
 
 using namespace cycfi;
-extern "C" {
-   bool                       artist_affine_transform_is_identity(affine_transform t) { return t.is_identity(); }
-   affine_transform           artist_affine_transform_translate(affine_transform t, double tx, double ty) { return t.translate(tx, ty); }
-   affine_transform           artist_affine_transform_scale(affine_transform t, double sx, double sy) { return t.scale(sx, sy); }
-   affine_transform           artist_affine_transform_scale_square(affine_transform t, double sc) { return t.scale(sc); }
-   inline affine_transform    artist_affine_transform_rotate(affine_transform t, double rad) { return t.rotate(rad); }
-   inline affine_transform    artist_affine_transform_skew(affine_transform t, double sx, double sy) { return t.skew(sx, sy); }
-   affine_transform           artist_affine_transform_invert(affine_transform t) { return t.invert(); }
 
-   point                      artist_affine_transform_apply_pt(affine_transform t, point p) { return t.apply(p); }
-   point                      artist_affine_transform_apply(affine_transform t, float x, float y) { return t.apply(x, y); }
+artist::affine_transform at(affine_transform t) {
+   return artist::affine_transform { t.a, t.b, t.c, t.d, t.tx, t.ty };
+}
+
+extern "C" {
+   affine_transform           artist_affine_transform_identity() { return artist::affine_identity; }
+   bool                       artist_affine_transform_is_identity(affine_transform t) { return at(t).is_identity(); }
+   affine_transform           artist_affine_transform_translate(affine_transform t, double tx, double ty) { return at(t).translate(tx, ty); }
+   affine_transform           artist_affine_transform_scale(affine_transform t, double sx, double sy) { return at(t).scale(sx, sy); }
+   affine_transform           artist_affine_transform_scale_square(affine_transform t, double sc) { return at(t).scale(sc); }
+   inline affine_transform    artist_affine_transform_rotate(affine_transform t, double rad) { return at(t).rotate(rad); }
+   inline affine_transform    artist_affine_transform_skew(affine_transform t, double sx, double sy) { return at(t).skew(sx, sy); }
+   affine_transform           artist_affine_transform_invert(affine_transform t) { return at(t).invert(); }
+
+   point                      artist_affine_transform_apply_pt(affine_transform t, point p)
+   { 
+      auto p_ = at(t).apply(artist::point(p.x, p.y));
+      return point { p_.x, p_.y };
+   }
+   point                      artist_affine_transform_apply(affine_transform t, float x, float y)
+   { 
+      auto p = at(t).apply(x, y);
+      return point { p.x, p.y };
+   }
 
    //                               template <std::size_t N>
    // void                       apply(point p[N]) const;
-   void                       artist_affine_transform_apply_points(affine_transform t, point* p, std::size_t n) { return t.apply(p, n); }
+   void                       artist_affine_transform_apply_points(affine_transform t, point* p, std::size_t n) { return at(t).apply((artist::point*)p, n); }
 
    affine_transform           artist_affine_transform_make_translation(double tx, double ty) { return artist::make_translation(tx, ty); }
    affine_transform           artist_affine_transform_make_scale(double sx, double sy) { return artist::make_scale(sx, sy); }
