8c9c395fa9301c10a59704ee9113c4c3ab7a753d
djowel <djowel@gmail.com>
2026-09-11 15:17:10 +0800

    text_run_test: use a u00e9 escape so MSVC accepts the char constant

--- test/text_run_test.cpp
+++ test/text_run_test.cpp
@@ -124,7 +124,7 @@ TEST_CASE("text_run: Constructors", "[text_run]")
    // UTF-8 is converted: indices are code points, not bytes.
    text_run a{fd40, "h\xc3\xa9llo"};
    CHECK(a.text().size() == 5);
-   CHECK(a.text()[1] == U'é');
+   CHECK(a.text()[1] == U'\u00e9');   // é
 
    text_run b{fd40, std::u32string_view{U"héllo"}};
    CHECK(b.text() == a.text());
